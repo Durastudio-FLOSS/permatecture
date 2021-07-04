2cf2655df0660d48080f1e60952c68ca7d63d8e9
Kelley Graham <1-root@users.noreply.gitlab.durastudio.com>
2021-07-04 13:46:06 -0700

    Add new file for bridging etc

---
+++ AtomicPi/wifi-bridge-install.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Setup secure wifi to ethernet bridge to provide full access point funtionality. MQTT broker needs full network intefration.
+# These commands are derived from https://www.thegeekpub.com/240065/setup-a-raspberry-pi-wireless-access-point/
