d06aa1c5dcf0aaaa41f9e144ff03aa57fd6245cf
root <kelley@durastudio.com>
2021-07-22 22:57:06 +0000

    Busybox UDHCPD replaces DHCPD

--- AtomicPi/wifi-bridge-install.sh
+++ AtomicPi/wifi-bridge-install.sh
@@ -6,7 +6,10 @@
 
 ETH_DEV=`ip link | awk -F: '$0 !~ "lo|vir|wl|do|^[^0-9]"{print $2;getline}'`
 WIFI_DEV=`ip link | awk -F: '$0 !~ "lo|vir|enp|eth|do|^[^0-9]"{print $2;getline}'`
-UDHCPD_CONF=/etc/default/udhcpd # edit for your distro
+
+# configure for your distro
+UDHCPD_CONF=/etc/default/udhcpd
+NET_CONFIG_FILE=/etc/network/interfaces.d/br0
 
 # uncomment and bring sysgtem up to date
 # sudo apt-get -y update && sudo apt-get -y upgrade
@@ -20,14 +23,25 @@ UDHCPD_CONF=/etc/default/udhcpd # edit for your distro
 #sudo sed -i s/'DHCPD_ENABLED="no"'/'DHCPD_ENABLED="yes"'/ $UDHCPD_CONF
 sed -i s/'DHCPD_ENABLED="no"'/'DHCPD_ENABLED="yes"'/ $UDHCPD_CONF
 #sudo systemctl restart udhcpd.service
-systemctl restart udhcpd.service
+#systemctl restart udhcpd.service
 
 # deny wifi udhcpd from assigning ip addresses to lan components
-if ! grep "denyinterfaces $ETH_DEV\ndenyinterfaces $WIFI_DEV" $UDHCPD_CONF ; then
+if ! grep "denyinterfaces $ETH_DEV" $UDHCPD_CONF ; then
    printf " - configuring $UDHCPD_CONF\n"
    #sudo printf "denyinterfaces $ETH_DEV\ndenyinterfaces $WIFI_DEV\n" >> $UDHCPD_CONF
    printf "denyinterfaces $ETH_DEV\ndenyinterfaces $WIFI_DEV\n" >> $UDHCPD_CONF
-   cat $UDHCPD_CONF | grep  deny
+   #cat $UDHCPD_CONF | grep  deny
+   #sudo systemctl restart udhcpd.service
+   systemctl restart udhcpd.service
 else
-   printf "NOTICE - $DHCPD_CONF already configured, skipping.\n"
+   printf "NOTICE - $UDHCPD_CONF already configured, skipping.\n"
 fi
+
+# add bridge block
+if ! grep "# AP Bridge setup" $NET_CONFIG_FILE ;  then
+   printf "# AP Bridge setup\nauto br0\niface br0 inet dhcp\nbridge_ports $ETH_DEV $WIFI_DEV\n" >> $NET_CONFIG_FILE
+   systemctl restart networking.service
+else
+   printf "NOTICE - $NET_CONFIG_FILE already configured, skipping.\n"
+fi
+
